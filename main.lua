04e9bf96578966814b73d35d8102adf210eced14
Breno Santos <brenosantos145@gmail.com>
2025-10-23 22:07:49 -0300

    feat: create main object and initial methods

---
+++ main.lua
@@ -0,0 +1,32 @@
+local InfoMessage = require("ui/widget/infomessage")
+local UIManager = require("ui/uimanager")
+local WidgetContainer = require("ui/widget/container/widgetcontainer")
+local _ = require("gettext")
+
+local ObsidianSync = WidgetContainer:extend({
+	name = "obsidiansync",
+	is_doc_only = false,
+})
+
+function ObsidianSync:init()
+	self.ui.menu:registerToMainMenu(self)
+end
+
+function ObsidianSync:addToMainMenu(menu_items)
+	menu_items.obsidiansync = {
+		text = _("Obsidian Sync"),
+		sorting_hint = "tools",
+		sub_item_table = {
+			{
+				text = _("Export Hightlights"),
+				callback = function()
+					UIManager:show(InfoMessage:new({
+						text = _("Hello, plugin world"),
+					}))
+				end,
+			},
+		},
+	}
+end
+
+return ObsidianSync
